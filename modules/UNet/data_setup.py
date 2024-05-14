da764dd3562e1a21bfa5e4f062919dc78dcb9a24
Luka Wong Chung <scur2224@int4.local.snellius.surf.nl>
2024-05-14 12:10:40 +0200

    made changes to data_setup

--- modules/UNet/data_setup.py
+++ modules/UNet/data_setup.py
@@ -2,6 +2,7 @@ import torch
 from torchvision import transforms
 import torchvision.transforms.functional as TF
 from torch.utils.data import Dataset, DataLoader
+import cv2
 import os
 
 class SegmentationDataset(Dataset):
@@ -13,10 +14,11 @@ class SegmentationDataset(Dataset):
         self.transform = transform
         
     def load_image(self, idx: int):
-        image_path = os.path.join(self.image_dir, self.filenames[idx])
+        filename_with_jpg = os.path.splitext(self.filenames[idx])[0] + '.jpg'
+        image_path = os.path.join(self.image_dir, filename_with_jpg)
         mask_path = os.path.join(self.mask_dir, self.filenames[idx])
         true_mask_path = os.path.join(self.true_mask_dir, self.filenames[idx])
-        return torch.load(image_path), torch.load(mask_path), torch.load(true_mask_path)
+        return cv2.imread(image_path), torch.load(mask_path), torch.load(true_mask_path)
     
     def __len__(self):
         return len(self.filenames)
@@ -28,10 +30,19 @@ class SegmentationDataset(Dataset):
         # image = torch.load(image_path)  # Load the image tensor
         # mask = torch.load(mask_path)    # Load the mask tensor
         image, mask, true_mask = self.load_image(idx)
-        input_tensor = torch.cat([image, mask], dim=0)  # Concatenating image and mask
+        image = torch.tensor(image, dtype=torch.float32)
+        mask = mask.permute(1, 2, 0)
+        true_mask = true_mask.permute(1, 2, 0)
 
         if self.transform:
-            input_tensor = self.transform(input_tensor)
-            true_mask = self.transform(true_mask)
+            # input_tensor = self.transform(input_tensor)
+            # true_mask = self.transform(true_mask)
+            transform = transforms.Compose([
+                transforms.Resize((256, 256)),  
+                transforms.ToTensor()  
+            ])
+            image = transform(image)
+            
+        input_tensor = torch.cat([image, mask], dim=0)  # Concatenating image and mask
 
         return input_tensor, true_mask
